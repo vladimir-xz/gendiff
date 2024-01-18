1c3b059e4c30be531becba1e632e9987735f4989
Vladimir <vladimir156xz@gmail.com>
2024-01-18 21:45:45 +0100

    new tests for nested arrays and files for them

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -12,13 +12,24 @@ class DifferTest extends TestCase
     {
         $firstFileJson = __DIR__ . "/fixtures/FlatOne.json";
         $secondFileJson = __DIR__ . "/fixtures/FlatTwo.json";
-        $result = file_get_contents(__DIR__ . "/fixtures/example.txt");
+        $resultFlat = file_get_contents(__DIR__ . "/fixtures/example.txt");
 
-        $this->assertEquals($result, genDiff($firstFileJson, $secondFileJson));
+        $this->assertEquals($resultFlat, genDiff($firstFileJson, $secondFileJson));
 
         $firstFileYml = __DIR__ . "/fixtures/FlatOne.yaml";
         $secondFileYml = __DIR__ . "/fixtures/FlatTwo.yaml";
 
-        $this->assertEquals($result, genDiff($firstFileYml, $secondFileYml));
+        $this->assertEquals($resultFlat, genDiff($firstFileYml, $secondFileYml));
+
+        $nestedFirstFileJson = __DIR__ . "/fixtures/NestedOne.json";
+        $nestedSecondFileJson = __DIR__ . "/fixtures/NestedTwo.json";
+        $resultNested = file_get_contents(__DIR__ . "/fixtures/exampleNested.txt");
+
+        $this->assertEquals($resultNested, genDiff($nestedFirstFileJson, $nestedSecondFileJson));
+
+        $nestedFirstFileYaml = __DIR__ . "/fixtures/NestedOne.yaml";
+        $nestedSecondFileYaml = __DIR__ . "/fixtures/NestedTwo.yaml";
+
+        $this->assertEquals($resultNested, genDiff($nestedFirstFileYaml, $nestedSecondFileYaml));
     }
 }
