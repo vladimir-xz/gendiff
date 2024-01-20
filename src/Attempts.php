37fbdb97d5958e703cb0776ed2815d62c141c555
Vladimir <vladimir156xz@gmail.com>
2024-01-20 21:52:28 +0100

    first try for nested array

--- src/Attempts.php
+++ src/Attempts.php
@@ -1,35 +1,39 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function Functional\sort;
+
+$nestedFirstFileJson = __DIR__ . "/../tests/fixtures/NestedOne.json";
+$nestedSecondFileJson = __DIR__ . "/../tests/fixtures/NestedTwo.json";
+$contentOne = file_get_contents($nestedFirstFileJson, true);
+$contentOne = json_decode($contentOne, true);
+$contentTwo = file_get_contents($nestedSecondFileJson, true);
+$contentTwo = json_decode($contentTwo, true);
+
+$jsonTwo = [ 'first' =>
+[    "timeout" => 20,
+    "verbose" => 'hello',
+    "host" => "hexlet.io"]
+];
+
+$jsonOne = [ 'first' =>
+[    "host" => "hexlet.io",
+    "timeout" => 50,
+    "proxy" => "123.234.53.22",
+     "follow" => "haha"]
+];
+
 $hi = [
-    "timeout" => 20,
-    "verbose" => true,
-    "host" => "hexlet.io",
-    "follow" => [
-        'ty' => 'da',
-        'ono' => 'nikogda',
-        'kdo' => 'ja'
-    ],
     "settings6" => [
-        "key" => "value",
+        "key" => true,
         "doge" => [
             "wow" => ""
         ]
     ],
-    "array" => [
-        'hello',
-        'ty kto'
-    ]
   ];
 
 $ku = [
-    "host" => "hexlet.io",
-    "timeout" => 50,
-    "proxy" => "123.234.53.22",
-    "follow" => [
-        'ty' => 'da',
-        'oni' => 'net',
-        'kdo' => 'on'
-    ],
     "settings5" => [
         "key5" => "value5"
     ],
@@ -40,13 +44,9 @@ $ku = [
             "wow" => "so much"
         ]
     ],
-    "array" => [
-        'one' => 'odin',
-        'two' => 'dva'
-    ]
 ];
 
-function ifBothArraysAreOfSameType($array1, $array2)
+function ifArraysOfSameType($array1, $array2)
 {
     if (array_key_exists(0, $array1) && !array_key_exists(0, $array2)) {
         return false;
@@ -59,27 +59,27 @@ function ifBothArraysAreOfSameType($array1, $array2)
 function lets($array1, $array2)
 {
     $merged = array_merge($array1, $array2);
-    if (!ifBothArraysAreOfSameType($array1, $array2)) {
+    if (!ifArraysOfSameType($array1, $array2)) {
         return $merged;
     }
     ksort($merged);
     $result = array_reduce(array_keys($merged), function ($acc, $key) use ($array1, $array2) {
         if (!array_key_exists($key, $array2)) {
-            $acc["- {$key}"] = $array1[$key];
+            $acc["- {$key}"] = json_encode($array1[$key], JSON_PRETTY_PRINT);
             return $acc;
         } elseif (!array_key_exists($key, $array1)) {
-            $acc["+ {$key}"] = $array2[$key];
+            $acc["+ {$key}"] = json_encode($array2[$key], JSON_PRETTY_PRINT);
             return $acc;
         } elseif ($array1[$key] === $array2[$key]) {
-            $acc["  {$key}"] = $array1[$key];
+            $acc["  {$key}"] = json_encode($array1[$key], JSON_PRETTY_PRINT);
             return $acc;
         } else {
-            if (is_array($array1[$key])) {
+            if (is_array($array1[$key]) && is_array($array2[$key])) {
                 $acc["  {$key}"] = lets($array1[$key], $array2[$key]);
                 return $acc;
             } else {
-                $acc["- {$key}"] = $array2[$key];
-                $acc["+ {$key}"] = $array1[$key];
+                $acc["- {$key}"] = json_encode($array1[$key], JSON_PRETTY_PRINT);
+                $acc["+ {$key}"] = json_encode($array2[$key], JSON_PRETTY_PRINT);
                 return $acc;
             }
         }
@@ -87,8 +87,24 @@ function lets($array1, $array2)
     return $result;
 }
 
-// $myarray = lets($hi, $ku);
-// var_dump($myarray);
+function printing($array, $separator = '    ', $depth = 1)
+{
+    $adding = str_repeat($separator, $depth);
+    $result = array_map(function ($key, $value) use ($separator, $depth, $adding) {
+        $result = "{$adding}{$key}";
+        if (is_array($value)) {
+            $depth += 1;
+            $value = printing($value, $separator, $depth);
+        }
+        $result .= ": {$value}";
+        return $result;
+    }, array_keys($array), $array);
+    $final = implode("\n", $result);
+    return "{\n{$final}\n{$adding}}";
+}
+
+$myarray = lets($contentOne, $contentTwo);
+echo printing($myarray);
 
 $data = [
     'hello' => 'world',
@@ -130,8 +146,78 @@ function showing($array)
         }
         return $item;
     }, $array);
-    $toJson = json_encode($result);
-    return "\n{$toJson}\n";
+    $toJson = json_encode($result, JSON_PRETTY_PRINT);
+    echo "{$toJson}";
+    return "{$toJson}";
 }
 
-echo (showing($data));
+
+function associative($array1, $array2)
+{
+    $merged = array_merge($array1, $array2);
+    if (!ifArraysOfSameType($array1, $array2)) {
+        return $merged;
+    }
+    //sort($merged, fn ($left, $right) => strcmp($left, $right));
+    $result = array_reduce(array_keys($merged), function ($acc, $key) use ($array1, $array2) {
+        if (!array_key_exists($key, $array2)) {
+            $acc[$key]['deleted'] = $array1[$key];
+            return $acc;
+        } elseif (!array_key_exists($key, $array1)) {
+            $acc[$key]['new'] = $array2[$key];
+            return $acc;
+        } elseif ($array1[$key] === $array2[$key]) {
+            $acc[$key]['same'] = $array1[$key];
+            return $acc;
+        } else {
+            if (is_array($array1[$key])) {
+                $acc[$key] = associative($array1[$key], $array2[$key]);
+                return $acc;
+            } else {
+                $acc[$key]['deleted'] = $array2[$key];
+                $acc[$key]['new'] = $array1[$key];
+                return $acc;
+            }
+        }
+    }, []);
+    return $result;
+}
+
+function getDeleted($array)
+{
+    return $array['deleted'] ?? null;
+}
+
+function getNew($array)
+{
+    return $array['new'] ?? null;
+}
+
+function getSame($array)
+{
+    return $array['same'] ?? null;
+}
+
+function showSmoker($array)
+{
+    $result = array_map(function ($key, $value) {
+        $new = getNew($value);
+        $old = getDeleted($value);
+        $same = getSame($value);
+        $result = [];
+        if ($old) {
+            $result[] = "- {$key}: {$old}";
+        }
+        if ($new) {
+            $result[] = "+ {$key}: {$new}\n";
+        }
+        if ($same) {
+            $result[] = "  {$key}: {$same}\n";
+        }
+        $result = implode("\n", $result);
+        echo $result;
+        return $result;
+    }, array_keys($array), $array);
+    $result = implode("\n", $result);
+    return $result;
+}
