8e69182ec368dbf04984cd2118894f30c5b91a73
Vladimir <vladimir156xz@gmail.com>
2024-01-21 20:24:31 +0100

    wrote tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -5,6 +5,8 @@ namespace Differ\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
+use function Differ\Stylish\printing;
+use function Differ\Parsers\parseFile;
 
 class DifferTest extends TestCase
 {
@@ -14,22 +16,22 @@ class DifferTest extends TestCase
         $secondFileJson = __DIR__ . "/fixtures/FlatTwo.json";
         $resultFlat = file_get_contents(__DIR__ . "/fixtures/example.txt");
 
-        $this->assertEquals($resultFlat, genDiff($firstFileJson, $secondFileJson));
+        $this->assertEquals($resultFlat, genDiff($firstFileJson, $secondFileJson, 'stylish'));
 
         $firstFileYml = __DIR__ . "/fixtures/FlatOne.yaml";
         $secondFileYml = __DIR__ . "/fixtures/FlatTwo.yaml";
 
-        $this->assertEquals($resultFlat, genDiff($firstFileYml, $secondFileYml));
+        $this->assertEquals($resultFlat, genDiff($firstFileYml, $secondFileYml, 'stylish'));
 
         $nestedFirstFileJson = __DIR__ . "/fixtures/NestedOne.json";
         $nestedSecondFileJson = __DIR__ . "/fixtures/NestedTwo.json";
         $resultNested = file_get_contents(__DIR__ . "/fixtures/exampleNested.txt");
 
-        $this->assertEquals($resultNested, genDiff($nestedFirstFileJson, $nestedSecondFileJson));
+        $this->assertEquals($resultNested, genDiff($nestedFirstFileJson, $nestedSecondFileJson, 'stylish'));
 
         $nestedFirstFileYaml = __DIR__ . "/fixtures/NestedOne.yaml";
         $nestedSecondFileYaml = __DIR__ . "/fixtures/NestedTwo.yaml";
 
-        $this->assertEquals($resultNested, genDiff($nestedFirstFileYaml, $nestedSecondFileYaml));
+        $this->assertEquals($resultNested, genDiff($nestedFirstFileYaml, $nestedSecondFileYaml, 'stylish'));
     }
 }
--- tests/ParsersTest.php
+++ tests/ParsersTest.php
@@ -4,7 +4,7 @@ namespace Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use function Differ\Processing\parseFile;
+use function Differ\Parsers\parseFile;
 
 class ParsersTest extends TestCase
 {
