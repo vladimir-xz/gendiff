77fee3c710b601b40aa2fcd0dec53c32316e0fdd
Vladimir <vladimir156xz@gmail.com>
2024-01-21 20:56:52 +0100

    clean up code in Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -5,29 +5,6 @@ namespace Differ\Differ;
 use function Differ\Parsers\parseFile;
 use function Differ\Stylish\printing;
 
-function FirstgenDiff($pathToFile1, $pathToFile2, $depth = 0)
-{
-    $firstFile = (array)parseFile($pathToFile1);
-    $secondFile = (array)parseFile($pathToFile2);
-    $mergedFiles = array_merge($secondFile, $firstFile);
-    ksort($mergedFiles);
-    $result = array_map(function ($key, $value) use ($firstFile, $secondFile) {
-        $stringValueOne = is_bool($value) ? var_export($value, true) : $value;
-        if (!array_key_exists($key, $secondFile)) {
-            return "  + {$key}: {$stringValueOne}";
-        } elseif (!array_key_exists($key, $firstFile)) {
-            return "  - {$key}: {$stringValueOne}";
-        } elseif ($value !== $secondFile[$key]) {
-            $stringValueTwo = is_bool($value) ? var_export($secondFile[$key], true) : $secondFile[$key];
-            return "  - {$key}: $stringValueOne\n  + {$key}: $stringValueTwo";
-        } else {
-            return "    {$key}: $stringValueOne";
-        }
-    }, array_keys($mergedFiles), array_values($mergedFiles));
-    $stringResult = implode("\n", $result);
-    return "{\n{$stringResult}\n}\n";
-}
-
 function ifArraysOfSameType($array1, $array2)
 {
     if (array_key_exists(0, $array1) && !array_key_exists(0, $array2)) {
