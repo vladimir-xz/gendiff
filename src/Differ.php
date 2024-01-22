e698e6d4ae46132d80b618d2421c845c4e93b018
Vladimir <vladimir156xz@gmail.com>
2024-01-22 22:28:02 +0100

    new way to create genDiff in stylish

--- src/Differ.php
+++ src/Differ.php
@@ -2,8 +2,54 @@
 
 namespace Differ\Differ;
 
+use Differ\Stylish;
+
 use function Differ\Parsers\parseFile;
-use function Differ\Stylish\printing;
+use function Differ\Stylish\showPrettyAssociative;
+
+$hi = [
+    "settings6" => [
+        "key" => true,
+        "doge" => [
+            "wow" => ""
+        ]
+    ],
+  ];
+
+$ku = [
+    "settings5" => [
+        "key5" => "value5"
+    ],
+    "settings6" => [
+        "key" => "value",
+        "ops" => "vops",
+        "doge" => [
+            "wow" => "so much"
+        ]
+    ],
+    "group3" => [
+        "deep" => [
+          "id" => [
+            "number" => 45
+          ]
+        ],
+        "fee" => 100500
+      ]
+];
+
+$jsonOne = [
+"host" => "hexlet.io",
+"timeout" => 50,
+"proxy" => "123.234.53.22",
+"follow" => false
+];
+
+$jsonTwo = [
+    "timeout" => 20,
+    "verbose" => false,
+    "host" => "hexlet.io"
+];
+
 
 function ifArraysOfSameType($array1, $array2)
 {
@@ -14,6 +60,62 @@ function ifArraysOfSameType($array1, $array2)
     }
     return true;
 }
+
+function addNewLine($array)
+{
+    return ['+' => $array];
+}
+
+function addDeletedLine($array)
+{
+    return ['-' => $array];
+}
+
+function addSameLine($array)
+{
+    return ['same' => $array];
+}
+
+function addChangedLine($array)
+{
+    return ['+/-' => $array];
+}
+
+function addOldAndNew($old, $new)
+{
+    return ['-' => $old, '+' => $new];
+}
+
+function compareAssociative($array1, $array2)
+{
+    $merged = array_merge($array1, $array2);
+    ksort($merged);
+    $result = array_reduce(array_keys($merged), function ($acc, $key) use ($array1, $array2) {
+        if (!array_key_exists($key, $array2)) {
+            $acc[$key] = addDeletedLine($array1[$key]);
+            return $acc;
+        } elseif (!array_key_exists($key, $array1)) {
+            $acc[$key] = addNewLine($array2[$key]);
+            return $acc;
+        } elseif ($array1[$key] === $array2[$key]) {
+            $acc[$key] = addSameLine($array1[$key]);
+            return $acc;
+        } else {
+            if (is_array($array1[$key]) && is_array($array2[$key])) {
+                $acc[$key] = addChangedLine(compareAssociative($array1[$key], $array2[$key]));
+                return $acc;
+            } else {
+                $acc[$key] = addOldAndNew($array1[$key], $array2[$key]);
+                return $acc;
+            }
+        }
+    }, []);
+    return $result;
+}
+
+// $one = compareAssociative($jsonOne, $jsonTwo);
+// // $rrr = Stylish\showPrettyAssociative($one);
+// echo $rrr;
 
 function compareArrays($array1, $array2)
 {
@@ -50,9 +152,9 @@ function genDiff($pathToFile1, $pathToFile2, $format)
 {
     $firstFile = (array)parseFile($pathToFile1);
     $secondFile = (array)parseFile($pathToFile2);
-    $array = compareArrays($firstFile, $secondFile);
+    $array = compareAssociative($firstFile, $secondFile);
     switch ($format) {
         case 'stylish':
-            return (printing($array));
+            return (showPrettyAssociative($array));
     }
 }
--- src/stylish.php
+++ src/stylish.php
@@ -11,6 +11,88 @@ function makeString(mixed $item)
     return $item;
 }
 
+function makeStringOfAssociative(mixed $item, $separator = '    ', $depth = 0)
+{
+    $adding = str_repeat($separator, $depth);
+    if (!is_array($item)) {
+        return trim(json_encode($item, JSON_PRETTY_PRINT), "\"");
+    }
+    $result = array_map(function ($key, $value) use ($separator, $depth) {
+        $depth += 1;
+        $adding = str_repeat($separator, $depth);
+        $stringValue = makeStringOfAssociative($value, $separator, $depth);
+        $result = "{$adding}{$key}: {$stringValue}";
+        return $result;
+    }, array_keys($item), $item);
+    $final = implode("\n", $result);
+    return "{\n{$final}\n{$adding}}";
+}
+
+function getNewLine($array)
+{
+    if (array_key_exists('+', $array)) {
+        return ['symbol' => '+ ', 'value' => $array['+']];
+    }
+    return 'NOT EXIST';
+}
+
+function getDeletedLine($array)
+{
+    if (array_key_exists('-', $array)) {
+        return ['symbol' => '- ', 'value' => $array['-']];
+    }
+    return 'NOT EXIST';
+}
+
+function getSameLine($array)
+{
+    if (array_key_exists('same', $array)) {
+        return ['symbol' => '  ', 'value' => $array['same']];
+    }
+    return 'NOT EXIST';
+}
+
+function getChangedLine($array)
+{
+    if (array_key_exists('+/-', $array)) {
+        return ['symbol' => '  ', 'value' => $array['+/-']];
+    }
+    return 'NOT EXIST';
+}
+
+function showPrettyAssociative($array, $separator = '    ', $depth = 0, $offset = 2)
+{
+    $adding = str_repeat($separator, $depth);
+    $result = array_map(function ($key, $value) use ($separator, $depth, $offset) {
+        $depth += 1;
+        $adding = substr(str_repeat($separator, $depth), $offset);
+        if (getDeletedLine($value) !== 'NOT EXIST' && getNewLine($value) !== 'NOT EXIST') {
+            ['symbol' => $symbolMinus, 'value' => $deletedValue] = getDeletedLine($value);
+            $deletedValue = makeStringOfAssociative($deletedValue, $separator, $depth, $offset);
+            ['symbol' => $symbolPlus, 'value' => $addedValue] = getNewLine($value);
+            $addedValue = makeStringOfAssociative($addedValue, $separator, $depth, $offset);
+            $line = "{$adding}{$symbolMinus}{$key}: {$deletedValue}\n{$adding}{$symbolPlus}{$key}: {$addedValue}";
+            return $line;
+        } elseif (getSameLine($value) !== 'NOT EXIST') {
+            ['symbol' => $symbol, 'value' => $newValue] = getSameLine($value);
+            $newValue = makeStringOfAssociative($newValue, $separator, $depth, $offset);
+        } elseif (getDeletedLine($value) !== 'NOT EXIST') {
+            ['symbol' => $symbol, 'value' => $newValue] = getDeletedLine($value);
+            $newValue = makeStringOfAssociative($newValue, $separator, $depth, $offset);
+        } elseif (getNewLine($value) !== 'NOT EXIST') {
+            ['symbol' => $symbol, 'value' => $newValue] = getNewLine($value);#
+            $newValue = makeStringOfAssociative($newValue, $separator, $depth, $offset);
+        } elseif (getChangedLine($value) !== 'NOT EXIST') {
+            ['symbol' => $symbol, 'value' => $newValue] = getChangedLine($value);
+            $newValue = showPrettyAssociative($newValue, $separator, $depth, $offset);
+        }
+        $line = "{$adding}{$symbol}{$key}: {$newValue}";
+        return $line;
+    }, array_keys($array), $array);
+    $final = implode("\n", $result);
+    return "{\n{$final}\n{$adding}}";
+}
+
 function printing($array, $separator = '    ', $depth = 0, $offset = 2)
 {
     $adding = str_repeat($separator, $depth);
