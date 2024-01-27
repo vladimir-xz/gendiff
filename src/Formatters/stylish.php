6a6dff47de359bc5135cce050928ad94daa772fb
Vladimir <vladimir156xz@gmail.com>
2024-01-28 00:50:25 +0100

    deleted Elvis operator and return version without empty line

--- src/Formatters/stylish.php
+++ src/Formatters/stylish.php
@@ -15,7 +15,7 @@ function makeString(mixed $item)
     return $item;
 }
 
-function stylishPrinting1(array $comparedArray, string $separator = '    ', int $depth = 0, int $offset = 2)
+function stylishPrinting(array $comparedArray, string $separator = '    ', int $depth = 0, int $offset = 2)
 {
     $adding = str_repeat($separator, $depth);
     $result = array_map(function ($key, $value) use ($separator, $depth, $offset) {
@@ -37,32 +37,4 @@ function stylishPrinting1(array $comparedArray, string $separator = '    ', int 
     }, array_keys($comparedArray), $comparedArray);
     $final = implode("\n", $result);
     return "{\n{$final}\n{$adding}}";
-}
-
-
-function stylishPrinting(array $comparedArray, string $separator = '    ', int $depth = 0, int $offset = 2)
-{
-    $iter = function ($value, $depth) use (&$iter, $separator, $offset) {
-        $endingSeparator = str_repeat($separator, $depth);
-        $lines = array_map(function ($key, $value) use (&$iter, $separator, $offset, $depth) {
-            $newDepth = $depth + 1;
-            if (in_array($key[0], ['+', '-', ' '], true)) {
-                $addingWithouOffset = str_repeat($separator, $newDepth);
-                $adding = substr($addingWithouOffset, $offset, null);
-            } else {
-                $adding = str_repeat($separator, $newDepth);
-            }
-            $keyString = "{$adding}{$key}";
-            if (is_array($value)) {
-                $convertedValue = $iter($value, $newDepth);
-            } else {
-                $convertedValue = makeString($value);
-            }
-            $stringResult = "{$keyString}: {$convertedValue}";
-            return $stringResult;
-        }, array_keys($value), $value);
-        return implode("\n", ["{", ...$lines, "{$endingSeparator}}"]);
-    };
-
-    return "{$iter($comparedArray, $depth)}";
 }
