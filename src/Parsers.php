8e492a7da6217716a3baa14cff44f7c3e093695a
Vladimir <vladimir156xz@gmail.com>
2024-01-27 18:02:14 +0100

    solving mutations again

--- src/Parsers.php
+++ src/Parsers.php
@@ -8,27 +8,21 @@ function parseFile(string $pathToFile)
 {
     if (!file_exists($pathToFile)) {
         throw new \Exception("File do not found: \"{$pathToFile}\"!");
-    }
-    if (filesize($pathToFile) == 0) {
+    } elseif (filesize($pathToFile) == 0) {
         $pathBaseName = pathinfo($pathToFile, PATHINFO_BASENAME);
         throw new \Exception("File \"{$pathBaseName}\" is empty.");
     }
     $fileExtention = pathinfo($pathToFile, PATHINFO_EXTENSION);
-    if ($fileExtention === 'yml') {
-        $fileExtention = 'yaml';
-    }
-    switch ($fileExtention) {
-        case 'json':
-            $content = file_get_contents($pathToFile, true);
-            if ($content === false) {
-                throw new \Exception("Unknow file extention: \"{$content}\"!");
-            }
-            $contentString = $content;
-            return json_decode($contentString, true);
-        case 'yaml':
-            $content = Yaml::parseFile($pathToFile);
-            return $content;
-        default:
-            throw new \Exception("Unknow file extention: \"{$fileExtention}\"!");
+    if ($fileExtention === 'json') {
+        $content = file_get_contents($pathToFile, true);
+        if ($content === false) {
+            throw new \Exception("Unknow file extention: \"{$content}\"!");
+        }
+        return json_decode($content, true);
+    } elseif ($fileExtention === 'yaml' || $fileExtention === 'yml') {
+        $content = Yaml::parseFile($pathToFile);
+        return $content;
+    } else {
+        throw new \Exception("Unknow file extention: \"{$fileExtention}\"!");
     }
 }
