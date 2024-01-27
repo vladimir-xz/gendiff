b983fa38fef656e47bf163a4405253fe2233d6bf
Vladimir <vladimir156xz@gmail.com>
2024-01-27 18:44:57 +0100

    making all path absolute

--- src/Parsers.php
+++ src/Parsers.php
@@ -4,23 +4,30 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
+// function ifPathIsAbsolute($pathToFile)
+// {
+//     $absolutePath = __DIR__ . $pathToFile;
+//     if
+// }
+
 function parseFile(string $pathToFile)
 {
-    if (!file_exists($pathToFile)) {
+    $absolutePath = realpath($pathToFile);
+    if ($absolutePath === false) {
         throw new \Exception("File do not found: \"{$pathToFile}\"!");
-    } elseif (filesize($pathToFile) == 0) {
-        $pathBaseName = pathinfo($pathToFile, PATHINFO_BASENAME);
+    } elseif (filesize($absolutePath) == 0) {
+        $pathBaseName = pathinfo($absolutePath, PATHINFO_BASENAME);
         throw new \Exception("File \"{$pathBaseName}\" is empty.");
     }
-    $fileExtention = pathinfo($pathToFile, PATHINFO_EXTENSION);
+    $fileExtention = pathinfo($absolutePath, PATHINFO_EXTENSION);
     if ($fileExtention === 'json') {
-        $content = file_get_contents($pathToFile, true);
+        $content = file_get_contents($absolutePath, true);
         if ($content === false) {
             throw new \Exception("Unknow file extention: \"{$content}\"!");
         }
         return json_decode($content, true);
     } elseif ($fileExtention === 'yaml' || $fileExtention === 'yml') {
-        $content = Yaml::parseFile($pathToFile);
+        $content = Yaml::parseFile($absolutePath);
         return $content;
     } else {
         throw new \Exception("Unknow file extention: \"{$fileExtention}\"!");
