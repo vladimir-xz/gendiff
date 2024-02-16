0c4759c297d2e3e43dda001d193a0bdff4351d9c
Vladimir <vladimir156xz@gmail.com>
2024-02-16 20:08:37 +0100

    changed parseFile to simple parse

--- src/Parsers.php
+++ src/Parsers.php
@@ -15,7 +15,7 @@ function makePathAbsolute(string $pathToFile)
     return $absolutePath;
 }
 
-function getFileContent(string $absolutePath)
+function getFilesContent(string $absolutePath)
 {
     if (!file_exists($absolutePath)) {
         throw new \Exception("File do not found: \"{$absolutePath}\"!");
@@ -29,15 +29,14 @@ function getFileContent(string $absolutePath)
 function parseFile(string $pathToFile)
 {
     $fileExtention = pathinfo($pathToFile, PATHINFO_EXTENSION);
-    $content = getFileContent($pathToFile);
+    $content = getFilesContent($pathToFile);
     if ($fileExtention === 'json') {
         if ($content === false) {
             throw new \Exception('Error when turning value into string');
         }
         return json_decode($content, true);
     } elseif ($fileExtention === 'yaml' || $fileExtention === 'yml') {
-        $contentOfYaml = Yaml::parseFile($pathToFile);
-        return $contentOfYaml;
+        return Yaml::parse($content);
     } else {
         throw new \Exception("Unknow file extention: \"{$fileExtention}\"!");
     }
