bbeb359bc131e47e07d86107a2d15e934b2d88a3
Vladimir <vladimir156xz@gmail.com>
2024-02-16 20:10:12 +0100

    corrected typo

--- tests/ParsersTest.php
+++ tests/ParsersTest.php
@@ -5,15 +5,14 @@ namespace Differ\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Parsers\parseFile;
-use function Differ\Parsers\makePathAbsolute;
-use function Differ\Parsers\getFileContent;
+use function Differ\Parsers\getFilesContent;
 
 class ParsersTest extends TestCase
 {
     public function testDoNotExistException(): void
     {
         $this->expectExceptionMessage("File do not found: \"/Notexist.txt\"!");
-        getFileContent('/Notexist.txt');
+        getFilesContent('/Notexist.txt');
     }
     public function testBadExtention(): void
     {
@@ -25,6 +24,6 @@ class ParsersTest extends TestCase
     {
         $empty = __DIR__ . "/fixtures/Empty.json";
         $this->expectExceptionMessage("File \"Empty.json\" is empty.");
-        getFileContent($empty);
+        getFilesContent($empty);
     }
 }
