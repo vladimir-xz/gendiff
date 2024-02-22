995e0044de969755662039f312865d845edb6ea2
vladimir-xz <vladimir156xz@gmail.com>
2024-02-22 23:48:32 +0300

    first try to solve json

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,26 +4,29 @@ namespace Differ\Formatters\Json;
 
 use function Differ\Differ\getNode;
 
-function printJson(array $comparedData): string
+function makeArrayForPrint(array $comparedData): array
 {
     $result = array_map(function ($data) {
-        ['status' => $status, 'symbol' => $symbol, 'difference' => $difference] = getNode($data);
+        ['status' => $status, 'difference' => $difference] = getNode($data);
         $key = key($difference);
         $value = current($difference);
         if ($status === 'old and new') {
             $oldAndNewValues = array_map(function ($node) use ($key) {
-                $stringValue = json_encode(current($node['difference']), 0, 512);
-                return "\"{$node['symbol']}{$key}\":{$stringValue}";
+                ['symbol' => $symbol, 'difference' => $difference] = getNode($node);
+                return ["{$symbol}{$key}" => current($difference)];
             }, $value);
-            return implode(',', $oldAndNewValues);
+            return array_merge(...$oldAndNewValues);
         } elseif ($status === 'changed') {
-            $innerJson = printJson($value);
-            return "\"{$key}\":{$innerJson}";
+            return [$key => makeArrayForPrint($value)];
         } else {
-            $valueJson = json_encode($value);
-            return "\"{$symbol}{$key}\":{$valueJson}";
+            return [$key => $value];
         }
     }, $comparedData);
-    $keysAndValuesInString = implode(',', $result);
-    return "{{$keysAndValuesInString}}";
+    return array_merge(...$result);
+}
+
+function printJson(array $comparedArray)
+{
+    $preparedArray = makeArrayForPrint($comparedArray);
+    return json_encode($preparedArray);
 }
