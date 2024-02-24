e8e0ebae2971c0a83cc208a6b68b285a438b039b
Vladimir <vladimir156xz@gmail.com>
2024-02-24 18:07:49 +0100

    added function getPathToFixtures in test

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,28 +8,29 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 
 use function Differ\Differ\genDiff;
-use function Differ\FilesProcessing\findDirectory;
 
-const FIXTURES_DIR = __DIR__ . '/fixtures/';
-// define("FIXTURES_DIR", findDirectory('fixtures'));
+function getPathToFixtures($path)
+{
+    return __DIR__  . "/fixtures/" . $path;
+}
 
 final class DifferTest extends TestCase
 {
     public static function additionProvider(): array
     {
-        $nestedFirstFileJson = "/NestedOne.json";
-        $nestedSecondFileJson = "/NestedTwo.json";
-        $resultNested = "/ExampleNested.txt";
-        $nestedFirstFileYaml = "/NestedOne.yaml";
-        $nestedSecondFileYaml = "/NestedTwo.yml";
-        $resultPlain = "/ResultPlain.txt";
-        $resultJson = "/ResultOfJson.json";
+        $nestedFirstFileJson = "NestedOne.json";
+        $nestedSecondFileJson = "NestedTwo.json";
+        $resultNested = "ExampleNested.txt";
+        $nestedFirstFileYaml = "NestedOne.yaml";
+        $nestedSecondFileYaml = "NestedTwo.yml";
+        $resultPlain = "ResultPlain.txt";
+        $resultJson = "ResultOfJson.json";
 
         return [
-            'Nested Json'  => [FIXTURES_DIR . $nestedFirstFileJson, FIXTURES_DIR . $nestedSecondFileJson, 'stylish', FIXTURES_DIR . $resultNested],
-            'Nested Yaml' => [FIXTURES_DIR . $nestedFirstFileYaml, FIXTURES_DIR . $nestedSecondFileYaml, 'stylish', FIXTURES_DIR . $resultNested],
-            'Plain' => [FIXTURES_DIR . $nestedFirstFileYaml, FIXTURES_DIR . $nestedSecondFileYaml, 'plain', FIXTURES_DIR . $resultPlain],
-            'Json output'  => [FIXTURES_DIR . $nestedFirstFileYaml, FIXTURES_DIR . $nestedSecondFileYaml, 'json', FIXTURES_DIR . $resultJson],
+            'Nested Json'  => [$nestedFirstFileJson, $nestedSecondFileJson, 'stylish',$resultNested],
+            'Nested Yaml' => [$nestedFirstFileYaml, $nestedSecondFileYaml, 'stylish', $resultNested],
+            'Plain' => [$nestedFirstFileYaml, $nestedSecondFileYaml, 'plain', $resultPlain],
+            'Json output'  => [$nestedFirstFileYaml, $nestedSecondFileYaml, 'json', $resultJson],
         ];
     }
 
@@ -38,6 +39,7 @@ final class DifferTest extends TestCase
      */
     public function testGenDiff(string $arrayOne, string $arrayTwo, string $format, string $expected): void
     {
-        $this->assertStringEqualsFile($expected, genDiff($arrayOne, $arrayTwo, $format));
+        $pathToFiles = array_map(fn ($file) => getPathToFixtures($file), [$arrayOne, $arrayTwo, $expected]);
+        $this->assertStringEqualsFile($pathToFiles[2], genDiff($pathToFiles[0], $pathToFiles[1], $format));
     }
 }
