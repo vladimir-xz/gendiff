b1dd7ef544e00ddb03a7f22164b1701e43e4a52a
Vladimir <vladimir156xz@gmail.com>
2024-02-27 23:53:24 +0100

    deleted useless lines use function in json module

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,8 +2,6 @@
 
 namespace Differ\Formatters\Json;
 
-use function Differ\Differ\getNode;
-
 function format(array $comparedArray): string
 {
     return json_encode($comparedArray, JSON_PRETTY_PRINT);
